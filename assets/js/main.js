616b210a17dca7ccac25dc9b9718a50b81eac6e4
Suhaib Sawalha <suhaibsawalha7@gmail.com>
2024-03-17 14:11:13 +0200

    add the About section

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,7 +8,7 @@ function applyHoverOnNavbar() {
           const lineNav = document.getElementById("lineNav");
           lineNav.style.display = "block";
           const rect = a.getBoundingClientRect();
-          lineNav.style.left = `${rect.left - 20}px`;
+          lineNav.style.left = `${rect.left - 10}px`;
           lineNav.style.width = `${rect.width}px`;
           lineNav.style.height = `3px`;
           lineNav.style.top = `45px`;
@@ -17,7 +17,7 @@ function applyHoverOnNavbar() {
         const backNav = document.getElementById("backNav");
         backNav.style.display = "block";
         const rect = a.getBoundingClientRect();
-        backNav.style.left = `${rect.left - 30}px`;
+        backNav.style.left = `${rect.left - 20}px`;
         backNav.style.width = `${rect.width + 20}px`;
         backNav.style.height = `${rect.height + 10}px`;
         backNav.style.top = `${rect.top - 5}px`;
@@ -37,7 +37,7 @@ function applyHoverOnNavbar() {
         a.style.textDecoration = "none";
         a.style.opacity = "1";
         const rect = a.getBoundingClientRect();
-        lineNav.style.left = `${rect.left - 20}px`;
+        lineNav.style.left = `${rect.left - 10}px`;
         lineNav.style.width = `${rect.width}px`;
         lineNav.style.height = `3px`;
         lineNav.style.top = `45px`;
@@ -59,7 +59,7 @@ function applyHoverOnNavbar() {
       a.style.textDecoration = "none";
       a.style.opacity = "1";
       const rect = a.getBoundingClientRect();
-      backNav.style.left = `${rect.left - 30}px`;
+      backNav.style.left = `${rect.left - 20}px`;
       backNav.style.width = `${rect.width + 20}px`;
       backNav.style.height = `${rect.height + 10}px`;
       backNav.style.top = `${rect.top - 5}px`;
@@ -117,7 +117,7 @@ const currentScroll = window.scrollY;
 function sectionsMove() {
   for (let i = 1; i < hrefs.length; ++i) {
     const a = document.querySelector(hrefs[i]);
-    if (this.window.scrollY > a.offsetTop - 350) {
+    if (this.window.scrollY > a.offsetTop - 500) {
       a.style.animation = "slideFromBottom 1s ease forwards";
     }
   }
@@ -125,3 +125,71 @@ function sectionsMove() {
 
 window.addEventListener("scroll", sectionsMove);
 window.addEventListener("load", sectionsMove);
+
+function customizeHeadLines() {
+  document.querySelectorAll(".headerLine").forEach((line) => {
+    const span = document.createElement("span");
+    span.textContent = line.previousElementSibling.textContent.trim();
+    span.style.fontSize = getComputedStyle(
+      line.previousElementSibling
+    ).fontSize;
+    span.style.visibility = "hidden";
+    document.body.appendChild(span);
+    line.style.width = `${span.offsetWidth + 20}px`;
+    document.body.removeChild(span);
+  });
+}
+
+window.addEventListener("resize", customizeHeadLines);
+window.addEventListener("load", customizeHeadLines);
+
+const mySkills = [
+  { name: "C++", level: "95%", image: "./../assets/img/skills/c++.png" },
+  { name: "C", level: "90%", image: "./../assets/img/skills/c.png" },
+  { name: "HTML", level: "95%", image: "./../assets/img/skills/html.png" },
+  { name: "CSS", level: "90%", image: "./../assets/img/skills/css.png" },
+  {
+    name: "JavaScript",
+    level: "80%",
+    image: "./../assets/img/skills/javascript.png",
+  },
+  {
+    name: "Bootstrap",
+    level: "95%",
+    image: "./../assets/img/skills/bootstrap.png",
+  },
+  { name: "Java", level: "75%", image: "./../assets/img/skills/java.png" },
+  { name: "Python", level: "65%", image: "./../assets/img/skills/python.png" },
+  { name: "Dart", level: "50%", image: "./../assets/img/skills/dart.png" },
+  { name: "Kotlin", level: "30%", image: "./../assets/img/skills/kotlin.png" },
+  {
+    name: "Flutter",
+    level: "65%",
+    image: "./../assets/img/skills/flutter.png",
+  },
+  { name: "React JS", level: "30%", image: "./../assets/img/skills/react.png" },
+  {
+    name: "React Native",
+    level: "20%",
+    image: "./../assets/img/skills/react.png",
+  },
+  { name: "Django", level: "20%", image: "./../assets/img/skills/django.png" },
+];
+
+for (const skill of mySkills) {
+  const div = document.createElement("div");
+  div.className = "skill col-12 col-md-6 col-lg-4 col-xl-3";
+  div.innerHTML = `
+    <div class="skill-body">
+      <div class="skill-image">
+      <img src="${skill.image}" alt="${skill.name}" width="60" height="60"/>
+      </div>
+      <div class="skill-name">${skill.name}</div>
+      <div class="skill-level d-flex align-items-center">
+        <div class="skill-level-bar" style="width: ${skill.level}"></div>
+        <div class="skill-level-percentage">${skill.level}</div>
+      </div>
+    </div>
+  `;
+  document.querySelector("#skills").appendChild(div);
+}
